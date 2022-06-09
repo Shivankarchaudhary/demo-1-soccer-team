7837e469eb2ca38e2cc7c8f46c9c625c0bb078a1
bhakti.rathod <bhakti.rathod@stackroute.in>
2022-06-09 12:00:57 +0530

    demo added

---
+++ src/main/java/com/jap/AgeDemo.java
@@ -0,0 +1,18 @@
+package com.jap;
+
+public class AgeDemo {
+    public static void main(String[] args) {
+        int age[] = new int[5];
+        age[0] = 30;
+        age[1] = 32;
+        age[2] = 34;
+        age[3] = 45;
+        age[4] = 34;
+
+        System.out.println("Index 0 : " + age[0]);
+        System.out.println("Index 1 : " + age[1]);
+        System.out.println("Index 2 : " + age[2]);
+        System.out.println("Index 3 : " + age[3]);
+        System.out.println("Index 4 : " + age[4]);
+    }
+}
